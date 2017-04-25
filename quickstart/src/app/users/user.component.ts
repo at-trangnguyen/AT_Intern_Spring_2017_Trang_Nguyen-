271896dde7076920ee7938d47771a402c5a50acf
at-trangnguyen <trang.nguyen@asiantech.vn>
2017-04-25 22:12:30 +0700

    Update

--- quickstart/src/app/users/user.component.ts
+++ quickstart/src/app/users/user.component.ts
@@ -2,47 +2,47 @@ import {Component, OnInit} from '@angular/core'
 import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
-	selector: 'my-app',
-	templateUrl: './user.html'
+  selector: 'my-app',
+  templateUrl: './user.html'
 })
 
 export class UserComponent implements OnInit {
-	public signupForm: FormGroup;
-	users: any[];
-	firstName = new FormControl(''); 
-	lastName = new FormControl('');
-	middleName = new FormControl('');
-	email = new FormControl('');
+  public signupForm: FormGroup;
+  users: any[];
+  firstName = new FormControl(''); 
+  lastName = new FormControl('');
+  middleName = new FormControl('');
+  email = new FormControl('');
 
-	constructor(private builder: FormBuilder) {
-		this.users =[];
-	}
+  constructor(private builder: FormBuilder) {
+    this.users =[];
+  }
 
-	ngOnInit() {
-		this.signupForm = this.builder.group({
-			firstname: this.firstName,
-			lastname: this.lastName,
-			middlename: this.middleName,
-			email: this.email
-		});
-		this.firstName.valueChanges.subscribe(data => {
-			this.emails();
-		});
-		this.lastName.valueChanges.subscribe(data => {
-			this.emails();
-		});
-	}
+  ngOnInit() {
+    this.signupForm = this.builder.group({
+      firstname: this.firstName,
+      lastname: this.lastName,
+      middlename: this.middleName,
+      email: this.email
+    });
+    this.firstName.valueChanges.subscribe(data => {
+      this.emails();
+    });
+    this.lastName.valueChanges.subscribe(data => {
+      this.emails();
+    });
+  }
 
-	emails() {
-		let mergeEmail = this.firstName.value +"."+ this.lastName.value + "@asiantech.vn";
-		if ( this.firstName.value && this.lastName.value ) {
-			this.email.setValue(mergeEmail);
-		}
-		else {
-			this.email.setValue('');
-		}
-	}
-	signup() {
-		this.users.push(this.signupForm.value);
-	}
+  emails() {
+    let mergeEmail = this.firstName.value +"."+ this.lastName.value + "@asiantech.vn";
+    if ( this.firstName.value && this.lastName.value ) {
+      this.email.setValue(mergeEmail);
+    }
+    else {
+      this.email.setValue('');
+    }
+ }
+  signup() {
+    this.users.push(this.signupForm.value);
+  }
 }
